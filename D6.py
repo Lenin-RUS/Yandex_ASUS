e430567ff62a02624437fd74b9c4e9ff438f5d7d
Lenin <S.Lanin@gmail.com>
2021-11-03 15:41:29 +0300

    1 попытка

--- D6.py
+++ D6.py
@@ -15,7 +15,7 @@ class Stack:
 
 class Queue:
     def __init__(self):
-        self.queue = [None] * v[0]
+        self.queue = []
         self.head = 0
         self.tail = 0
         self.size = 0
@@ -24,23 +24,49 @@ class Queue:
         return self.size == 0
 
     def push(self, x):
-        self.queue[self.tail] = x
+        self.queue.append(x)
         self.tail += 1
         self.size += 1
 
     def pop(self):
         x = self.queue[self.head]
-        self.queue[self.head] = None
+        # self.queue[self.head] = None
         self.head += 1
         self.size -= 1
         return x
 
 
-v = [int(i) for i in input().split(' ')]
-# v[0] - количество вершин
-# v[1] - количество ребер
-e = []
+def BFS(s):
+    planned = Queue()
+    planned.push(s)
+    color[s] = 'gray'
+    distance[s]=0
+    while not planned.is_empty():
+        u = planned.pop()  # Возьмём вершину из очереди.
+        if u in graph:
+            for v in graph[u]:
+                if color[v] == 'white':  # Серые и чёрные вершины уже
+                    distance[v] = distance[u] + 1
+                    previous[v] = u
+                    color[v] = 'gray'
+                    planned.push(v)  # Запланируем посещение вершины.
+            color[u] = 'black'  # Теперь вершина считается обработанной.
+    return planned.queue
+
+
+def ShortestPath(v):  # Кратчайший путь от s до v.
+    path = Stack()
+    current_vertex = v
+    while current_vertex != None:  # Предшественник вершины s равен None.
+        path.push(current_vertex)
+        current_vertex = previous[current_vertex]
+    return path
+
 graph = dict()
+
+v = [int(i) for i in input().split(' ')]
+e = []
+
 for i in range(v[1]):
     e = [int(i) for i in input().split(' ')]
     if e[0] in graph:
@@ -52,34 +78,20 @@ for i in range(v[1]):
         graph[e[1]].append(e[0])
     else:
         graph[e[1]] = [e[0]]
-start_vertex = 1
+start_vertex = int(input())
 
-color = ['white'] * (v[0])
-previous = [None] * (v[0])
-distance = [None] * (v[0])
+# v=[6,6]
+# graph = {4: [2, 1], 2: [4, 3, 6], 1: [4, 5], 5: [1, 6], 3: [2], 6: [5, 2]}
+# start_vertex = 6
 
+for i in graph:
+    graph[i].sort()
 
-def BFS(s):
-    planned = Queue()
-    planned.push(s)
-    color[s] = 'gray'
-    distance[s] = 0
-    while not planned.is_empty():
-        u = planned.pop()  # Возьмём вершину из очереди.
-        if u in graph:
-            for v in graph[u]:
-                if color[v] == 'white':  # Серые и чёрные вершины уже
-                    distance[v] = distance[u] + 1
-                previous[v] = u
-                color[v] = 'gray'
-                planned.push(v)  # Запланируем посещение вершины.
-        color[u] = 'black'  # Теперь вершина считается обработанной.
+color = ['white'] * (v[0]+1)
+previous = [None] * (v[0]+1)
+distance = [None] * (v[0]+1)
 
 
-def ShortestPath(v):  # Кратчайший путь от s до v.
-    path = Stack()
-    current_vertex = v
-    while current_vertex != None:  # Предшественник вершины s равен None.
-        path.push(current_vertex)
-        current_vertex = previous[current_vertex]
-    return path
+final_dist = BFS(start_vertex)
+print(*final_dist)
+
