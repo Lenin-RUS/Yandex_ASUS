e7c04e8c69a5ffc5c60c82cfffe26f954cc4683d
Lenin <S.Lanin@gmail.com>
2021-11-03 14:48:43 +0300

    1 попытка

--- D6.py
+++ D6.py
@@ -1,4 +1,4 @@
-class Stack():
+class Stack:
     def __init__(self):
         self.values = []
         self.colors = dict()
@@ -12,7 +12,33 @@ class Stack():
     def is_empty(self):
         return len(self.values)
 
+
+class Queue:
+    def __init__(self):
+        self.queue = [None] * v[0]
+        self.head = 0
+        self.tail = 0
+        self.size = 0
+
+    def is_empty(self):
+        return self.size == 0
+
+    def push(self, x):
+        self.queue[self.tail] = x
+        self.tail += 1
+        self.size += 1
+
+    def pop(self):
+        x = self.queue[self.head]
+        self.queue[self.head] = None
+        self.head += 1
+        self.size -= 1
+        return x
+
+
 v = [int(i) for i in input().split(' ')]
+# v[0] - количество вершин
+# v[1] - количество ребер
 e = []
 graph = dict()
 for i in range(v[1]):
@@ -26,40 +52,34 @@ for i in range(v[1]):
         graph[e[1]].append(e[0])
     else:
         graph[e[1]] = [e[0]]
-start_vertex = int(input())
+start_vertex = 1
+
+color = ['white'] * (v[0])
+previous = [None] * (v[0])
+distance = [None] * (v[0])
 
 
-# Длины массивов равны числу вершин|V|.
-color = [white, white, ...]
-previous = [None, None, ...]
-distance = [None, None, ...]
-
-функция BFS(s):
-    # Создадим очередь вершин и положим туда стартовую вершину.
+def BFS(s):
     planned = Queue()
     planned.push(s)
-    color[s] = gray
+    color[s] = 'gray'
     distance[s] = 0
-    пока очередь planned не пуста:
-        u = planned.pop() # Возьмём вершину из очереди.
-        для каждого ребра (u,v), исходящего из u:
-            возьмём вершину v
-            if color[v] == white: # Серые и чёрные вершины уже
-                                  # либо в очереди, либо обработаны.
-                distance[v] = distance[u] + 1
+    while not planned.is_empty():
+        u = planned.pop()  # Возьмём вершину из очереди.
+        if u in graph:
+            for v in graph[u]:
+                if color[v] == 'white':  # Серые и чёрные вершины уже
+                    distance[v] = distance[u] + 1
                 previous[v] = u
-                color[v] = gray
-                planned.push(v) # Запланируем посещение вершины.
-        color[u] = black # Теперь вершина считается обработанной.
+                color[v] = 'gray'
+                planned.push(v)  # Запланируем посещение вершины.
+        color[u] = 'black'  # Теперь вершина считается обработанной.
 
-функция ShortestPath(v): # Кратчайший путь от s до v.
-    # Класть вершины будем в стек, тогда
-    # стартовая вершина окажется наверху стека
-    # и порядок следования от s до v будет соответствовать
-    # порядку извлечения вершин из стека.
+
+def ShortestPath(v):  # Кратчайший путь от s до v.
     path = Stack()
     current_vertex = v
-    while current_vertex != None: # Предшественник вершины s равен None.
+    while current_vertex != None:  # Предшественник вершины s равен None.
         path.push(current_vertex)
         current_vertex = previous[current_vertex]
     return path
