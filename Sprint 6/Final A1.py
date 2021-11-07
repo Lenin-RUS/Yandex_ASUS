d10a79862624b6ff74fe75f9e332a884afec1272
Lenin <S.Lanin@gmail.com>
2021-11-07 23:26:08 +0300

    Lenin PC

--- Sprint 6/Final A1.py
+++ Sprint 6/Final A1.py
@@ -1,7 +1,11 @@
+# import sys
+# sys.setrecursionlimit(2000)
+
 def edge_add(key):
     index = len(edges)
     edges.append(key)
     sift_up(index)
+
 
 def sift_up(index):
     if index == 1:
@@ -12,6 +16,7 @@ def sift_up(index):
             edges[parent_index], edges[index] = edges[index], edges[parent_index]
             sift_up(parent_index)
 
+
 def pop_max():
     result = edges[1]
     edges[1] = edges[-1]
@@ -19,12 +24,13 @@ def pop_max():
     sift_down(1)
     return result
 
+
 def sift_down(index):
     left = 2 * index
     right = 2 * index + 1
     if len(edges) <= left:
-        return None
-    if (right < len(edges)) and (edges_arr[edges[left]][2]  < edges_arr[edges[right]][2]):
+        return 1
+    if (right < len(edges)) and (edges_arr[edges[left]][2] < edges_arr[edges[right]][2]):
         index_largest = right
     else:
         index_largest = left
@@ -32,6 +38,7 @@ def sift_down(index):
     if edges_arr[edges[index]][2] < edges_arr[edges[index_largest]][2]:
         edges[index], edges[index_largest] = edges[index_largest], edges[index]
         sift_down(index_largest)
+
 
 def add_vertex(v):
     added.add(v)
@@ -42,6 +49,7 @@ def add_vertex(v):
         if edges_arr[i][1] in not_added:
             edge_add(i)
 
+
 def find_MST():
     v = 0
     final_sum = 0
@@ -49,7 +57,7 @@ def find_MST():
     while not_added and edges:
         e = pop_max()
         if edges_arr[e][1] in not_added:
-            max_spanning_tree.append(e)
+            # max_spanning_tree.append(e)
             final_sum += edges_arr[e][2]
             add_vertex(edges_arr[e][1])
 
@@ -59,36 +67,38 @@ def find_MST():
         print(final_sum)
 
 
-# nod, edg = (int(i) for i in input().split(' '))
-# e = []
-# edges_arr = dict()
-# nodes_arr = dict()
-# for i in range(nod):
-#     nodes_arr[i] = []
+nod, edg = (int(i) for i in input().split(' '))
+e = []
+edges_arr = dict()
+nodes_arr = dict()
+for i in range(nod):
+    nodes_arr[i] = []
+
+count = 0
+for i in range(edg):
+    start, end, w = (int(i) - 1 for i in input().split(' '))
+    nodes_arr[start].append(count)
+    nodes_arr[end].append(count + edg)
+    edges_arr[count] = [start, end, w + 1]
+    edges_arr[count + edg] = [end, start, w + 1]
+    count += 1
+
+# nod, edg = 6, 9
+# edges_arr = {0: [0, 4, 6], 9: [4, 0, 6], 1: [0, 1, 2],
+#              10: [1, 0, 2], 2: [1, 5, 7], 11: [5, 1, 7],
+#              3: [5, 4, 8], 12: [4, 5, 8], 4: [4, 3, 4],
+#              13: [3, 4, 4], 5: [3, 5, 3], 14: [5, 3, 3],
+#              6: [5, 2, 1], 15: [2, 5, 1], 7: [2, 1, 5],
+#              16: [1, 2, 5], 8: [2, 3, 9], 17: [3, 2, 9]}
 #
-# count = 0
-# for i in range(edg):
-#     start, end, w = (int(i) - 1 for i in input().split(' '))
-#     nodes_arr[start].append(count)
-#     nodes_arr[end].append(count+edg)
-#     edges_arr[count] = [start, end, w + 1]
-#     edges_arr[count + edg] = [end, start, w + 1]
-#     count += 1
+# nodes_arr = {0: [0, 1], 1: [10, 2, 16], 2: [15, 7, 8], 3: [13, 5, 17], 4: [9, 12, 4], 5: [11, 3, 14, 6]}
 
-
-nod, edg = 6, 9
-edges_arr = {0: [0, 4, 6], 9: [4, 0, 6], 1: [0, 1, 2],
-             10: [1, 0, 2], 2: [1, 5, 7], 11: [5, 1, 7],
-             3: [5, 4, 8], 12: [4, 5, 8], 4: [4, 3, 4],
-             13: [3, 4, 4], 5: [3, 5, 3], 14: [5, 3, 3],
-             6: [5, 2, 1], 15: [2, 5, 1], 7: [2, 1, 5],
-             16: [1, 2, 5], 8: [2, 3, 9], 17: [3, 2, 9]}
-
-nodes_arr = {0: [0, 1], 1: [10, 2, 16], 2: [15, 7, 8], 3: [13, 5, 17], 4: [9, 12, 4], 5: [11, 3, 14, 6]}
-
-max_spanning_tree = []  # Рёбра, составляющие MST.
+# max_spanning_tree = []  # Рёбра, составляющие MST.
 added = set()  # Множество вершин, уже добавленных в остов.
 not_added = set(range(nod))  # Множество вершины, ещё не добавленных в остов.
 edges = [-1]  # Массив рёбер, исходящих из остовного дерева.
 
-find_MST()
+if edg == 0 and nod != 1:
+    print('Oops! I did it again')
+else:
+    find_MST()
--- Temp/my_solution.py
+++ Temp/my_solution.py
@@ -1,31 +1,5 @@
-# nod, edg = (int(i) for i in input().split(' '))
-# e = []
-#
-# edges_arr = dict()
-# nodes_arr = dict()
+def test_1(x):
+    return None
 
-# for i in range(nod):
-#     nodes_arr[i]=[]
-#
-# count = 0
-# for i in range(edg):
-#     start, end, w = (int(i)-1 for i in input().split(' '))
-#     nodes_arr[start].append(count)
-#     nodes_arr[end].append(count+edg)
-#     edges_arr[count] = [start, end, w+1]
-#     edges_arr[count+edg] = [end, start, w+1]
-#     count+=1
 
-nod, edg = 6, 9
-edges_arr = {0: [0, 4, 6], 9: [4, 0, 6], 1: [0, 1, 2],
-             10: [1, 0, 2], 2: [1, 5, 7], 11: [5, 1, 7],
-             3: [5, 4, 8], 12: [4, 5, 8], 4: [4, 3, 4],
-             13: [3, 4, 4], 5: [3, 5, 3], 14: [5, 3, 3],
-             6: [5, 2, 1], 15: [2, 5, 1], 7: [2, 1, 5],
-             16: [1, 2, 5], 8: [2, 3, 9], 17: [3, 2, 9]}
-nodes_arr = {0: [0, 1], 1: [10, 2, 16], 2: [15, 7, 8], 3: [13, 5, 17], 4: [9, 12, 4], 5: [11, 3, 14, 6]}
-
-added = set()  # Множество вершин, уже добавленных в остов.
-not_added = set(range(nod))  # Множество вершины, ещё не добавленных в остов.
-edges = set()  # Массив рёбер, исходящих из остовного дерева.
-
+test_1(20)
