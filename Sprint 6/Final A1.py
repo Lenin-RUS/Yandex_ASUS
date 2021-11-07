be714fe4471b4f0a4643a67e709b229a00553604
Lenin <S.Lanin@gmail.com>
2021-11-07 22:58:45 +0300

    Lenin PC

--- Sprint 6/Final A1.py
+++ Sprint 6/Final A1.py
@@ -1,3 +1,38 @@
+def edge_add(key):
+    index = len(edges)
+    edges.append(key)
+    sift_up(index)
+
+def sift_up(index):
+    if index == 1:
+        return edges
+    else:
+        parent_index = index // 2
+        if edges_arr[edges[parent_index]][2] < edges_arr[edges[index]][2]:
+            edges[parent_index], edges[index] = edges[index], edges[parent_index]
+            sift_up(parent_index)
+
+def pop_max():
+    result = edges[1]
+    edges[1] = edges[-1]
+    edges.pop()
+    sift_down(1)
+    return result
+
+def sift_down(index):
+    left = 2 * index
+    right = 2 * index + 1
+    if len(edges) <= left:
+        return None
+    if (right < len(edges)) and (edges_arr[edges[left]][2]  < edges_arr[edges[right]][2]):
+        index_largest = right
+    else:
+        index_largest = left
+
+    if edges_arr[edges[index]][2] < edges_arr[edges[index_largest]][2]:
+        edges[index], edges[index_largest] = edges[index_largest], edges[index]
+        sift_down(index_largest)
+
 def add_vertex(v):
     added.add(v)
     not_added.discard(v)
@@ -5,26 +40,14 @@ def add_vertex(v):
 
     for i in nodes_arr[v]:
         if edges_arr[i][1] in not_added:
-            edges.add(i)
-
-
-def extract_minimum():
-    current_max = float('-inf')
-    current_max_edge = None
-    for v in edges:
-        if edges_arr[v][2] > current_max:
-            current_max = edges_arr[v][2]
-            current_max_edge = v
-    edges.discard(current_max_edge)
-    return current_max_edge
-
+            edge_add(i)
 
 def find_MST():
     v = 0
     final_sum = 0
     add_vertex(v)
     while not_added and edges:
-        e = extract_minimum()
+        e = pop_max()
         if edges_arr[e][1] in not_added:
             max_spanning_tree.append(e)
             final_sum += edges_arr[e][2]
@@ -36,36 +59,36 @@ def find_MST():
         print(final_sum)
 
 
-nod, edg = (int(i) for i in input().split(' '))
-e = []
-edges_arr = dict()
-nodes_arr = dict()
-for i in range(nod):
-    nodes_arr[i] = []
-
-count = 0
-for i in range(edg):
-    start, end, w = (int(i) - 1 for i in input().split(' '))
-    nodes_arr[start].append(count)
-    nodes_arr[end].append(count+edg)
-    edges_arr[count] = [start, end, w + 1]
-    edges_arr[count + edg] = [end, start, w + 1]
-    count += 1
-
-
-# nod, edg = 6, 9
-# edges_arr = {0: [0, 4, 6], 9: [4, 0, 6], 1: [0, 1, 2],
-#              10: [1, 0, 2], 2: [1, 5, 7], 11: [5, 1, 7],
-#              3: [5, 4, 8], 12: [4, 5, 8], 4: [4, 3, 4],
-#              13: [3, 4, 4], 5: [3, 5, 3], 14: [5, 3, 3],
-#              6: [5, 2, 1], 15: [2, 5, 1], 7: [2, 1, 5],
-#              16: [1, 2, 5], 8: [2, 3, 9], 17: [3, 2, 9]}
+# nod, edg = (int(i) for i in input().split(' '))
+# e = []
+# edges_arr = dict()
+# nodes_arr = dict()
+# for i in range(nod):
+#     nodes_arr[i] = []
 #
-# nodes_arr = {0: [0, 1], 1: [10, 2, 16], 2: [15, 7, 8], 3: [13, 5, 17], 4: [9, 12, 4], 5: [11, 3, 14, 6]}
+# count = 0
+# for i in range(edg):
+#     start, end, w = (int(i) - 1 for i in input().split(' '))
+#     nodes_arr[start].append(count)
+#     nodes_arr[end].append(count+edg)
+#     edges_arr[count] = [start, end, w + 1]
+#     edges_arr[count + edg] = [end, start, w + 1]
+#     count += 1
+
+
+nod, edg = 6, 9
+edges_arr = {0: [0, 4, 6], 9: [4, 0, 6], 1: [0, 1, 2],
+             10: [1, 0, 2], 2: [1, 5, 7], 11: [5, 1, 7],
+             3: [5, 4, 8], 12: [4, 5, 8], 4: [4, 3, 4],
+             13: [3, 4, 4], 5: [3, 5, 3], 14: [5, 3, 3],
+             6: [5, 2, 1], 15: [2, 5, 1], 7: [2, 1, 5],
+             16: [1, 2, 5], 8: [2, 3, 9], 17: [3, 2, 9]}
+
+nodes_arr = {0: [0, 1], 1: [10, 2, 16], 2: [15, 7, 8], 3: [13, 5, 17], 4: [9, 12, 4], 5: [11, 3, 14, 6]}
 
 max_spanning_tree = []  # Рёбра, составляющие MST.
 added = set()  # Множество вершин, уже добавленных в остов.
 not_added = set(range(nod))  # Множество вершины, ещё не добавленных в остов.
-edges = set()  # Массив рёбер, исходящих из остовного дерева.
+edges = [-1]  # Массив рёбер, исходящих из остовного дерева.
 
 find_MST()
--- Temp/main.py
+++ Temp/main.py
@@ -1,6 +1,56 @@
-a=dict()
-a[1]=[]
-a[2]=[]
-a[1].append([1,2])
-a[1].append([1,2])
-print(a)
+def edge_add(key):
+    index = len(edges)
+    edges.append(key)
+    sift_up(index)
+
+def sift_up(index):
+    if index == 1:
+        return edges
+    else:
+        parent_index = index // 2
+        if edges_arr[edges[parent_index]][2] < edges_arr[edges[index]][2]:
+            edges[parent_index], edges[index] = edges[index], edges[parent_index]
+            sift_up(parent_index)
+
+def pop_max():
+    result = edges[1]
+    edges[1] = edges[-1]
+    edges.pop()
+    sift_down(1)
+    return result
+
+def sift_down(index):
+    left = 2 * index
+    right = 2 * index + 1
+    if len(edges) <= left:
+        return None
+    if (right < len(edges)) and (edges_arr[edges[left]][2]  < edges_arr[edges[right]][2]):
+        index_largest = right
+    else:
+        index_largest = left
+
+    if edges_arr[edges[index]][2] < edges_arr[edges[index_largest]][2]:
+        edges[index], edges[index_largest] = edges[index_largest], edges[index]
+        sift_down(index_largest)
+
+
+
+
+edges_arr = {0: [0, 4, 6], 9: [4, 0, 6], 1: [0, 1, 2],
+             10: [1, 0, 2], 2: [1, 5, 7], 11: [5, 1, 7],
+             3: [5, 4, 8], 12: [4, 5, 8], 4: [4, 3, 4],
+             13: [3, 4, 4], 5: [3, 5, 3], 14: [5, 3, 3],
+             6: [5, 2, 1], 15: [2, 5, 1], 7: [2, 1, 5],
+             16: [1, 2, 5], 8: [2, 3, 9], 17: [3, 2, 9]}
+
+edges=[-1]
+for i in range(10):
+    edge_add(i)
+print(edges)
+
+print(edges)
+for i in edges[1:]:
+    print(edges_arr[i][2], end=' ')
+print()
+
+print(edges_arr[pop_max()][2])
