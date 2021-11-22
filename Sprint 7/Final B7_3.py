10fb115fbc10ce1b326089c6afe1f2b68e48cb16
Lenin <S.Lanin@gmail.com>
2021-11-22 12:38:35 +0300

    Lenin PC

--- Sprint 7/Final B7_3.py
+++ Sprint 7/Final B7_3.py
@@ -1,20 +1,25 @@
-# _ = input()
-# arr = [int(i) for i in input().split(' ')]
-arr = [3, 1, 1, 2, 2, 1]
+_ = input()
+arr = [int(i) for i in input().split(' ')]
+# arr = [1, 3, 1, 2, 2, 1]
 k = sum(arr)
 if k % 2 == 1:
     print("False")
 else:
     k = int(k / 2)
-    result = [[1 for _ in range(len(arr) + 1)] for _ in range(k + 1)]
+    # result = [[1 for _ in range(len(arr) + 1)] for _ in range(k + 1)]
+    A = [1 for _ in range(k + 1)]
+    B = [0 for _ in range(k + 1)]
+    B[0] = 1
     for i in range(1, k + 1):
-        result[i][0] = 0
         for j in range(1, len(arr) + 1):
-
-            if i - arr[j - 1] >= 0:
-                result[i][j] = (result[i][j - 1] or result[i - arr[j - 1]][j - 1])
+            if j % 2 == 1:
+                if i - arr[j - 1] >= 0:
+                    A[i] = (B[i] or B[i - arr[j - 1]])
+                else:
+                    A[i] = B[i]
             else:
-                result[i][j] = result[i][j - 1]
-    # for i in result:
-    #     print(*i)
-    print(result[-1][-1] == 1)
+                if i - arr[j - 1] >= 0:
+                    B[i] = (A[i] or A[i - arr[j - 1]])
+                else:
+                    B[i] = A[i]
+    print((A[-1] or B[-1]) == 1)
